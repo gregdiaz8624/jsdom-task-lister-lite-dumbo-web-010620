f91a537c084acc4f98d074cb3817ac3b50862c70
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-02-21 18:52:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+   const list = document.querySelector("#tasks")
+   const task = document.getElementById("create-task-form")
+
+   task.addEventListener("submit", event => {
+
+      event.preventDefault()
+
+      let taskInfo = document.querySelector("#new-task-description").value
+      
+      let newList = document.createElement("li")
+      newList.innerHTML = taskInfo + `<button class='delete'>X</button>`
+      list.appendChild(newList)
+
+      const deleteButton = newList.querySelector(".delete")
+      
+      deleteButton.addEventListener("click", (e) => {
+            newList.remove()
+      })
+   
+   })
+
+
+
+
 });
+//  const button = document.querySelector("#Clicker")
+//  button.addEventListener("click", function(event) {
+//     event.preventDefault()
+
+//  const list = document.querySelector("#list")
+//             console.log("test?")
+
